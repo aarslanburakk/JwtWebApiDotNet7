145f601396c3b3f3210ff4eb7f4527f964595a65
Burak Arslan <arslan.burak0909@gmail.com>
2023-08-01 00:46:50 +0300

    Version 1.0

---
+++ JwtWebApi/JwtWebApi/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using BCrypt.Net;
+using JwtWebApi.DTOs;
+using JwtWebApi.Models;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Drawing;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User User = new User();
+        public readonly IConfiguration _configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto data)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(data.Password);
+            User.PasswordHash = passwordHash;
+            User.UserName = data.UserName;
+            return Ok(User);
+
+        }
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto data)
+        {
+            if (User.UserName != data.UserName)
+            {
+                return BadRequest("User Not Found");
+            }
+            if (!BCrypt.Net.BCrypt.Verify(data.Password, User.PasswordHash))
+            {
+                return BadRequest("Wrong Password");
+            }
+            string token = CreateToken(User);
+            return Ok(token);
+        }
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim> {
+
+            new Claim(ClaimTypes.UserData, user.UserName),
+
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+       
+    }
+}
---
+++ JwtWebApi/JwtWebApi/DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtWebApi.DTOs
+{
+    public class UserDto
+    {
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+    }
+}
